e2f588527f240f5d881d5b629ced07a0fedbb1c4
은영 강 <lkjoi09@daum.net>
2022-12-22 08:17:07 +0000

    fist commit

---
+++ webapp/controller/App.controller.js
@@ -0,0 +1,14 @@
+sap.ui.define(
+    [
+        "sap/ui/core/mvc/Controller"
+    ],
+    function(BaseController) {
+      "use strict";
+  
+      return BaseController.extend("sessac.vaccine.controller.App", {
+        onInit() {
+        }
+      });
+    }
+  );
+  
---
+++ webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"sessac/vaccine/test/unit/controller/Main.controller"
+], function () {
+	"use strict";
+});
